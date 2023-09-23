087cae97f8e718c77da065f6ad7dfaf3a7ff0790
UlisesCH <ulisescoreas90@gmail.com>
2023-09-23 14:10:40 -0600

    Reestructura

---
+++ lib/app/view/home.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String _currentUrl = 'https://virtual.fmoues.edu.sv'; // URL
+
+  @override
+
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  Widget build(BuildContext context) {
+    return WebView(
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: _currentUrl,
+    );
+  }
+}
